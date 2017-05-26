661d95bcf348091dfdd1d9073eb34d6dbada1d96
ChihJuLai <eric55660712@gmail.com>
2017-05-26 23:30:48 +0800

    Test Method 3

---
+++ Test Method/CalculatorPlus_rotateScreen/src/androidTest/java/com/android/calculator2/CalculatorTest2.java
@@ -0,0 +1,95 @@
+package com.android.calculator2;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.IdlingPolicies;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CalculatorTest2 {
+    @Rule
+    public ActivityTestRule<Calculator> mActivityTestRule = new ActivityTestRule<>(Calculator.class);
+
+    @Test
+    @Repeat(times = 10)
+    public void calculatorTest2() {
+        ViewInteraction button = onView(
+                allOf(withId(R.id.digit_1), withText("1"),
+                        withParent(withId(R.id.pad_numeric)),
+                        isDisplayed()));
+        button.perform(click());
+
+
+
+        ViewInteraction calculatorEditText = onView(
+                allOf(withId(R.id.formula),
+                        withParent(withId(R.id.display)),
+                        isDisplayed()));
+        calculatorEditText.perform(replaceText("1"), closeSoftKeyboard());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.op_add), withText("+"), withContentDescription("plus"),
+                        withParent(withId(R.id.pad_operator)),
+                        isDisplayed()));
+        button2.perform(click());
+       // rotateScreen();
+        ViewInteraction calculatorEditText2 = onView(
+                allOf(withId(R.id.formula), withText("1"),
+                        withParent(withId(R.id.display)),
+                        isDisplayed()));
+        calculatorEditText.perform(replaceText("1+"), closeSoftKeyboard());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.digit_2), withText("2"),
+                        withParent(withId(R.id.pad_numeric)),
+                        isDisplayed()));
+        button3.perform(click());
+
+        ViewInteraction calculatorEditText3 = onView(
+                allOf(withId(R.id.formula), withText("1+"),
+                        withParent(withId(R.id.display)),
+                        isDisplayed()));
+        calculatorEditText.perform(replaceText("1+2"), closeSoftKeyboard());
+        rotateScreen();
+       // Thread.sleep(5000);
+    }
+
+    private void rotateScreen() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        int orientation = context.getResources().getConfiguration().orientation;
+
+        Activity activity = mActivityTestRule.getActivity();
+        activity.setRequestedOrientation(
+                (orientation == Configuration.ORIENTATION_PORTRAIT) ?
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+
+}
