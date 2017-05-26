f63f1870edd3a560ae341a6664cc3861df35e50e
ChihJuLai <eric55660712@gmail.com>
2017-05-26 23:19:49 +0800

    Test Method

---
+++ Test Method/CalculatorPlus_IntentServiceIdlingResource/CalculatorPlus/src/androidTest/java/com/android/calculator2/SuiteTest.java
@@ -0,0 +1,30 @@
+package com.android.calculator2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({CalculatorTest2.class, CalculatorTest3.class})
+public class SuiteTest {
+
+    @After
+    public void tearDown() throws Exception {
+
+        //try {
+           // Thread.sleep(5000);
+            //Ca = new CalculatorTest3.class;
+       // } catch (InterruptedException e) {
+        //   e.printStackTrace();
+      //  }
+
+
+
+
+        }
+    }
